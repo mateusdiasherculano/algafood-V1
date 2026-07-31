9003cef366e56e3a8f6ac60756bc87d09e403f22
mateus <mateusdias.her@gmail.com>
2026-07-31 20:50:18 +0000

    refactor: remove unnecessary blank lines in CidadeRepository and CozinhaRepository interfaces

--- algafood-api/src/main/java/com/algaworks/algafood_api/domain/repository/CidadeRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/domain/repository/CidadeRepository.java
@@ -2,7 +2,6 @@ package com.algaworks.algafood_api.domain.repository;
 import com.algaworks.algafood_api.domain.model.Cidade;
 import java.util.List;
 
-
 public interface CidadeRepository {
 
     List<Cidade> listar();
--- algafood-api/src/main/java/com/algaworks/algafood_api/domain/repository/CozinhaRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/domain/repository/CozinhaRepository.java
@@ -2,7 +2,6 @@ package com.algaworks.algafood_api.domain.repository;
 import com.algaworks.algafood_api.domain.model.Cozinha;
 import java.util.List;
 
-
 public interface CozinhaRepository {
 
     List<Cozinha> listar();
