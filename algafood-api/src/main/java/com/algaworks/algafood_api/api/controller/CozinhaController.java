cda0ba8ce2b78f8f650fa077520e228af2a81b88
mateus <mateusdias.her@gmail.com>
2026-08-01 17:38:34 +0000

    feat: add buscar method to CozinhaController for retrieving a specific Cozinha by ID

--- algafood-api/src/main/java/com/algaworks/algafood_api/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/api/controller/CozinhaController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/cozinhas")
@@ -21,6 +23,11 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.listar(); 
+    }
+
+    @GetMapping("/{id}")
+    public Cozinha buscar(@PathVariable Long id) {
+        return cozinhaRepository.buscar(id);
     }
 
 }
