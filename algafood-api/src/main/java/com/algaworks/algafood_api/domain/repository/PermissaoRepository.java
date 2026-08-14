e758a2c5d0412326886166cc1647e0aef8268745
mateus <mateusdias.her@gmail.com>
2026-08-14 14:18:33 +0000

    refactor: simplify PermissaoRepository by extending JpaRepository and remove custom implementation

--- algafood-api/src/main/java/com/algaworks/algafood_api/domain/repository/PermissaoRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/domain/repository/PermissaoRepository.java
@@ -1,11 +1,6 @@
 package com.algaworks.algafood_api.domain.repository;
 import com.algaworks.algafood_api.domain.model.Permissao;
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PermissaoRepository {
-
-    List<Permissao> listar();
-    Permissao buscar(Long id);
-    Permissao salvar(Permissao permissao);
-    void remover(Permissao permissao);
+public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
 }
