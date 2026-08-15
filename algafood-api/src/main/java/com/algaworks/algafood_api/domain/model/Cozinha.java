d28b5db81689118869b535f529177228c497377c
mateus <mateusdias.her@gmail.com>
2026-08-15 17:58:05 +0000

    feat: add one-to-many relationship between Cozinha and Restaurante entities

--- algafood-api/src/main/java/com/algaworks/algafood_api/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/domain/model/Cozinha.java
@@ -3,6 +3,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -19,4 +26,8 @@ public class Cozinha {
 
 	@Column(nullable = false)
 	private String nome;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 }
