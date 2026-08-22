8da3b65b7aeaef68cca12b0bdeaa5092f5924fa1
mateus <mateusdias.her@gmail.com>
2026-08-22 15:17:59 +0000

    feat: remove unused imports in EstadoController for cleaner code

--- algafood-api/src/main/java/com/algaworks/algafood_api/api/controller/EstadoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/api/controller/EstadoController.java
@@ -1,15 +1,11 @@
 package com.algaworks.algafood_api.api.controller;
 import com.algaworks.algafood_api.domain.repository.EstadoRepository;
-import com.algaworks.algafood_api.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafood_api.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood_api.domain.model.Estado;
 import com.algaworks.algafood_api.domain.service.EstadoService;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
