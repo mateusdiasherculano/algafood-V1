bc6afabdac7959325147976700a7293366c9ca55
mateus <mateusdias.her@gmail.com>
2026-08-22 15:01:38 +0000

    feat: add FetchType.LAZY to cidade relationship in Endereco and ensure descricao is non-null in FormaPagamento and Permissao

--- algafood-api/src/main/java/com/algaworks/algafood_api/domain/model/FormaPagamento.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/domain/model/FormaPagamento.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood_api.domain.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,5 +18,6 @@ public class FormaPagamento {
     @EqualsAndHashCode.Include
     private Long id;
     
+    @Column(nullable = false)
     private String descricao;
 }
--- algafood-api/src/main/java/com/algaworks/algafood_api/domain/model/Permissao.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/domain/model/Permissao.java
@@ -21,6 +21,7 @@ public class Permissao {
     @Column(nullable = false)
     private String nome;
     
+    @Column(nullable = false)
     private String descricao;
 
 }
