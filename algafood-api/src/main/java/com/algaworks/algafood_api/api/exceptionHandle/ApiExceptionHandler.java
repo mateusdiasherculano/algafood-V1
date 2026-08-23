f4321d3d15feb758bbccd5d92616fc85255b6e3f
mateus <mateusdias.her@gmail.com>
2026-08-23 20:58:22 +0000

    feat: add userMessage field to Problem class and refactor ApiExceptionHandler for consistent error messaging

--- algafood-api/src/main/java/com/algaworks/algafood_api/api/exceptionHandle/ApiExceptionHandler.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/api/exceptionHandle/ApiExceptionHandler.java
@@ -26,13 +26,15 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 
+	private static final String MSG_ERRO_GENERICO_USUARIO_FINAL = "Ocorreu um erro interno inesperado no sistema. "
+			+ "Tente novamente e se o problema persistir, entre em contato "
+			+ "com o administrador do sistema.";
+
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<Object> handleUncaught(Exception ex, WebRequest request) {
 		HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;		
 		ProblemType problemType = ProblemType.ERRO_DE_SISTEMA;
-		String detail = "Ocorreu um erro interno inesperado no sistema. "
-				+ "Tente novamente e se o problema persistir, entre em contato "
-				+ "com o administrador do sistema.";
+		String detail = MSG_ERRO_GENERICO_USUARIO_FINAL;
 		ex.printStackTrace();
 		
 		Problem problem = createProblemBuilder(status, problemType, detail).build();
@@ -108,7 +110,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		String detail = String.format("A propriedade '%s' não existe. "
 				+ "Corrija ou remova essa propriedade e tente novamente.", path);
 
-		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		Problem problem = createProblemBuilder(status, problemType, detail)
+			.userMessage(MSG_ERRO_GENERICO_USUARIO_FINAL).build();
 		
 		return handleExceptionInternal(ex, problem, headers, status, request);
 	}
@@ -123,7 +126,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 				+ "que é de um tipo inválido. Corrija e informe um valor compatível com o tipo %s.",
 				path, ex.getValue(), ex.getTargetType().getSimpleName());
 		
-		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		Problem problem = createProblemBuilder(status, problemType, detail)
+			.userMessage(MSG_ERRO_GENERICO_USUARIO_FINAL).build();
 		
 		return handleExceptionInternal(ex, problem, headers, status, request);
 	}
@@ -149,7 +153,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		ProblemType problemType = ProblemType.ENTIDADE_EM_USO;
 		String detail = ex.getMessage();
 		
-		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		Problem problem = createProblemBuilder(status, problemType, detail)
+			.userMessage(detail).build();
 		
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
