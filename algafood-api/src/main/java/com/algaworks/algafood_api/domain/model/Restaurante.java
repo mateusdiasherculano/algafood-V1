137ab7d710b4f648af78be78185c61f20c617a3b
mateus <mateusdias.her@gmail.com>
2026-08-24 18:36:40 +0000

    feat: add custom message to NotBlank annotation for nome field in Restaurante model

--- algafood-api/src/main/java/com/algaworks/algafood_api/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood_api/domain/model/Restaurante.java
@@ -37,7 +37,7 @@ public class Restaurante {
 	@EqualsAndHashCode.Include
 	private Long id;
 
-	@NotBlank
+	@NotBlank(message = "O nome do restaurante é obrigatório")
     @Column(nullable = false)
 	private String nome;
 
